7e7d4db2b547c1fcbefa4134c9aac53077765e7b
s34141 <s34141@pjwstk.edu.pl>
2025-05-30 22:23:06 +0200

    Zadanie drugie

--- src/Car.java
+++ src/Car.java
@@ -2,10 +2,22 @@ public class Car extends Vehicle {
     private int numberOfSeats;
 
     public Car(int numberOfSeats) {
+
         this.numberOfSeats = numberOfSeats;
     }
 
     public int getNumberOfSeats() {
+
         return numberOfSeats;
     }
+
+    @Override
+    public void Start() {
+        System.out.println("Samochód został uruchomiony...");
+    }
+    @Override
+    public void Stop() {
+        System.out.println("Samochód się zatrzymał");
+    }
 }
+
